67f9b10656934110e0030bc6dd32f0071ce1ec5f
Hasan Setiawan <hasan.setiawan@kodefox.com>
2016-10-23 14:43:28 +0700

    add type for option item

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -4,13 +4,18 @@ import {TouchableOpacity, View, Text, Modal, ScrollView, TouchableWithoutFeedbac
 import styles from './Dropdown-style';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+type Option = {
+  value: string | number | Object | Function;
+  label: string;
+}
+
 type Props = {
   onSelect: Function;
   label?: string;
   onShow: Function;
-  isShowingOptions: boolean,
-  selectedOption: string | Object;
-  options: Array<any>;
+  isShowingOptions: boolean;
+  selectedOption: Option;
+  options: Array<Option>;
   animationType: 'none' | 'fade' | 'slide';
 };
 
@@ -20,7 +25,7 @@ export default function Dropdown(props: Props) {
       {props.label ? <Text style={styles.label}>{props.label}</Text> : null}
       <TouchableOpacity style={styles.button} onPress={() => props.onShow(true)}>
         <View style={styles.option}>
-          <Text style={styles.defaultOptionText}>{props.selectedOption}</Text>
+          <Text style={styles.defaultOptionText}>{props.selectedOption.label}</Text>
           <Icon name="expand-more" style={styles.icon} />
         </View>
       </TouchableOpacity>
@@ -40,15 +45,15 @@ export function OptionsModal(props: Props) {
     >
       <TouchableWithoutFeedback onPress={() => props.onShow(false)}>
         <View style={styles.backdrop}>
-        <ScrollView style={styles.scrollViewOption}>
-          {props.options.map((item, index) => {
-            return (
-              <TouchableOpacity key={index} style={styles.optionItem} onPress={() => props.onSelect(item, false)}>
-                <Text style={styles.optionText}>{item}</Text>
-              </TouchableOpacity>
-            );
-          })}
-        </ScrollView>
+          <ScrollView style={styles.scrollViewOption}>
+            {props.options.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} style={styles.optionItem} onPress={() => props.onSelect(item, false)}>
+                  <Text style={styles.optionText}>{item.label}</Text>
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
         </View>
       </TouchableWithoutFeedback>
     </Modal>
--- src/__tests__/Dropdown-test.js
+++ src/__tests__/Dropdown-test.js
@@ -12,9 +12,14 @@ describe('Dropdown tests', () => {
   let onSelect = createSpy();
   let onShow = createSpy();
   let dropProps = {
-    defaultValue: 'Jakarta',
-    options: ['Bandung', 'Surabaya', 'Palembang'],
-    selectedOption: 'Bandung',
+    defaultValue: {value: 5, label: 'Serpong'},
+    options: [
+      {value: 1, label: 'Bandung'},
+      {value: 2, label: 'Surabaya'},
+      {value: 3, label: 'Palembang'},
+      {value: 4, label: 'Jakarta'},
+    ],
+    selectedOption: {value: 5, label: 'Serpong'},
     isShowingOptions: true,
     onSelect,
     onShow,
@@ -30,7 +35,7 @@ describe('Dropdown tests', () => {
     expect(wrapper.find(Icon).length).toBe(1);
 
     let dropdownWrapper = wrapper.find(OptionsModal).shallow().find(ScrollView).children();
-    expect(dropdownWrapper.length).toBe(3);
+    expect(dropdownWrapper.length).toBe(4);
     expect(dropdownWrapper.at(0).props().children.props.children).toBe('Bandung');
     expect(dropdownWrapper.at(1).props().children.props.children).toBe('Surabaya');
     expect(dropdownWrapper.at(2).props().children.props.children).toBe('Palembang');
@@ -43,19 +48,24 @@ describe('Dropdown tests', () => {
     expect(onSelect).toNotHaveBeenCalled();
     dropdownWrapper.at(0).simulate('press');
     expect(onSelect).toHaveBeenCalled();
-    expect(onSelect.calls[0].arguments[0]).toBe('Bandung');
+    expect(onSelect.calls[0].arguments[0]).toEqual({label: 'Bandung', value: 1});
     expect(onSelect.calls[0].arguments[1]).toBe(false);
 
     dropProps = {
-      defaultValue: 'Jakarta',
-      options: ['Bandung', 'Surabaya', 'Palembang'],
-      selectedOption: 'Surabaya',
+      defaultValue: {value: 5, label: 'Serpong'},
+      options: [
+        {value: 1, label: 'Bandung'},
+        {value: 2, label: 'Surabaya'},
+        {value: 3, label: 'Palembang'},
+        {value: 4, label: 'Jakarta'},
+      ],
+      selectedOption: {value: 5, label: 'Serpong'},
       isShowingOptions: true,
       onSelect,
       onShow,
       animationType: 'none',
     };
     wrapper = shallow(<Dropdown {...dropProps} />);
-    expect(wrapper.find(Text).at(0).props().children).toBe('Surabaya');
+    expect(wrapper.find(Text).at(0).props().children).toBe('Serpong');
   });
 });
